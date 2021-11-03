9d31266aa314c982b200cbbd953114f55251162a
roaco <roaco_91@hotmail.com>
2021-11-03 01:13:00 -0500

    Se implementa datadriven pero continua con errores

--- src/main/java/co/com/choucair/certification/pruebatecnica/tasks/RegisterUser.java
+++ src/main/java/co/com/choucair/certification/pruebatecnica/tasks/RegisterUser.java
@@ -3,6 +3,7 @@ package co.com.choucair.certification.pruebatecnica.tasks;
 import co.com.choucair.certification.pruebatecnica.userinterface.UTestJoinTodayPage;
 import co.com.choucair.certification.pruebatecnica.userinterface.UTestRegisterUser;
 import net.serenitybdd.screenplay.Actor;
+import net.serenitybdd.screenplay.Performable;
 import net.serenitybdd.screenplay.Task;
 import net.serenitybdd.screenplay.Tasks;
 import net.serenitybdd.screenplay.actions.Click;
@@ -16,30 +17,57 @@ import java.security.Key;
 public class RegisterUser implements Task {
 
     private String registeredUser;
+    private String strFirstName;
+    private String strLastName;
+    private String strEmailAddress;
+    private String strMonthBirth;
+    private String strDayBirth;
+    private String strYearBirth;
+    private String strInputLanguage;
+    private String strInputCity;
+    private String strInputZIP;
+    private String strInputCountry;
+    private String strPassword;
+    private String strConfirmPassword;
 
+    public RegisterUser(String strFirstName, String strLastName, String strEmailAddress, String strMonthBirth, String strDayBirth, String strYearBirth, String strInputLanguage, String strInputCity, String strInputZIP, String strInputCountry, String strPassword, String strConfirmPassword) {
+        this.strFirstName = strFirstName;
+        this.strLastName = strLastName;
+        this.strEmailAddress = strEmailAddress;
+        this.strMonthBirth = strMonthBirth;
+        this.strDayBirth = strDayBirth;
+        this.strYearBirth = strYearBirth;
+        this.strInputLanguage = strInputLanguage;
+        this.strInputCity = strInputCity;
+        this.strInputZIP = strInputZIP;
+        this.strInputCountry = strInputCountry;
+        this.strPassword = strPassword;
+        this.strConfirmPassword = strConfirmPassword;
+    }
 
     public RegisterUser(String registeredUser) {
         this.registeredUser = registeredUser;
     }
 
-    public static RegisterUser the(String registeredUser){
+    public static RegisterUser the(String strFirstName, String strLastName, String strEmailAddress, String strMonthBirth, String strDayBirth, String strYearBirth, String strInputLanguage, String strInputCity, String strInputZIP, String strInputCountry, String strPassword, String registeredUser){
         return Tasks.instrumented(RegisterUser.class, registeredUser);
     }
+
     @Override
     public <T extends Actor> void performAs(T actor) {
-            actor.attemptsTo( Enter.theValue("Rodrigo").into(UTestRegisterUser.INPUT_FIRST_NAME),
-                Enter.theValue("Acosta Restrepo").into(UTestRegisterUser.INPUT_LAST_NAME),
-                Enter.theValue("prueba@utest.com").into(UTestRegisterUser.INPUT_EMAIL_ADDRESS),
-                SelectFromOptions.byVisibleText("January").from(UTestRegisterUser.SELECT_MONTH),
-                SelectFromOptions.byVisibleText("11").from(UTestRegisterUser.SELECT_DAY),
-                SelectFromOptions.byVisibleText("1991").from(UTestRegisterUser.SELECT_YEAR),
+            actor.attemptsTo( Enter.theValue(strFirstName).into(UTestRegisterUser.INPUT_FIRST_NAME),
+                Enter.theValue(strLastName).into(UTestRegisterUser.INPUT_LAST_NAME),
+                Enter.theValue(strEmailAddress).into(UTestRegisterUser.INPUT_EMAIL_ADDRESS),
+                SelectFromOptions.byVisibleText(strMonthBirth).from(UTestRegisterUser.SELECT_MONTH),
+                SelectFromOptions.byVisibleText(strDayBirth).from(UTestRegisterUser.SELECT_DAY),
+                SelectFromOptions.byVisibleText(strYearBirth).from(UTestRegisterUser.SELECT_YEAR),
                 Click.on(UTestRegisterUser.INPUT_LANGUAGES),
                 Click.on(UTestRegisterUser.NEXT_BUTTON_LOCATION),
-                Enter.theValue("Armenia").into(UTestRegisterUser.INPUT_CITY),
+                Enter.theValue(strInputCity).into(UTestRegisterUser.INPUT_CITY),
                 Hit.the(Keys.ARROW_DOWN).into(UTestRegisterUser.INPUT_CITY),
                 Hit.the(Keys.ENTER).into(UTestRegisterUser.INPUT_CITY),
-                Enter.theValue("630001").into(UTestRegisterUser.INPUT_POSTAL_CODE),
-                SelectFromOptions.byVisibleText("Colombia").from(UTestRegisterUser.SELECT_COUNTRY),
+                Enter.theValue(strInputZIP).into(UTestRegisterUser.INPUT_POSTAL_CODE),
+                SelectFromOptions.byVisibleText(strInputCountry).from(UTestRegisterUser.SELECT_COUNTRY),
                 //Click.on(UTestRegisterUser.SELECT_COUNTRY),
                 Click.on(UTestRegisterUser.NEXT_BUTTON_DEVICES),
                 Click.on(UTestRegisterUser.SELECT_OS_COMPUTER),
@@ -49,8 +77,8 @@ public class RegisterUser implements Task {
                 Click.on(UTestRegisterUser.SELECT_MODEL),
                 Click.on(UTestRegisterUser.SELECT_OS_MOBILE),
                 Click.on(UTestRegisterUser.NEXT_BUTTON_LAST_STEP),
-                Enter.theValue(registeredUser).into(UTestRegisterUser.CREATE_PASSWORD),
-                Enter.theValue(registeredUser).into(UTestRegisterUser.CONFIRM_PASSWORD),
+                Enter.theValue(strPassword).into(UTestRegisterUser.CREATE_PASSWORD),
+                Enter.theValue(strConfirmPassword).into(UTestRegisterUser.CONFIRM_PASSWORD),
                 Click.on(UTestRegisterUser.CONFIRM_STAY_INFORMED),
                 Click.on(UTestRegisterUser.CONFIRM_TERMS),
                 Click.on(UTestRegisterUser.CONFIRM_PRIVACY),
--- src/test/java/co/com/choucair/certification/pruebatecnica/stepdefinitions/uTestStepDefinitions.java
+++ src/test/java/co/com/choucair/certification/pruebatecnica/stepdefinitions/uTestStepDefinitions.java
@@ -1,5 +1,6 @@
 package co.com.choucair.certification.pruebatecnica.stepdefinitions;
 
+import co.com.choucair.certification.pruebatecnica.model.UTestData;
 import co.com.choucair.certification.pruebatecnica.questions.Answer;
 import co.com.choucair.certification.pruebatecnica.tasks.Login;
 import co.com.choucair.certification.pruebatecnica.tasks.OpenUp;
@@ -12,6 +13,8 @@ import net.serenitybdd.screenplay.GivenWhenThen;
 import net.serenitybdd.screenplay.actors.OnStage;
 import net.serenitybdd.screenplay.actors.OnlineCast;
 
+import java.util.List;
+
 public class uTestStepDefinitions {
     @Before
     public void setStage(){
@@ -19,20 +22,33 @@ public class uTestStepDefinitions {
     }
 
     @Given("^than Rodrigo wants to register in uTest$")
-    public void thanRodrigoWantsToRegisterInUTest() {
+    public void thanRodrigoWantsToRegisterInUTest() throws Exception{
         OnStage.theActorCalled("Rodrigo").wasAbleTo(OpenUp.thePage(), (Login.onThePage()));
 
     }
 
     @When("^He clicks on the (.*) button$")
-    public void heClicksOnTheJoinTodayButton(String registeredUser) {
-        OnStage.theActorInTheSpotlight().attemptsTo(RegisterUser.the(registeredUser));
+    public void heClicksOnTheJoinTodayButton(List<UTestData> uTestData) throws Exception {
+        OnStage.theActorInTheSpotlight().attemptsTo(RegisterUser.the(
+                uTestData.get(0).getStrFirstName(),
+                uTestData.get(0).getStrLastName(),
+                uTestData.get(0).getStrEmailAddress(),
+                uTestData.get(0).getStrMonthBirth(),
+                uTestData.get(0).getStrDayBirth(),
+                uTestData.get(0).getStrYearBirth(),
+                uTestData.get(0).getStrInputLanguage(),
+                uTestData.get(0).getStrInputCity(),
+                uTestData.get(0).getStrInputZIP(),
+                uTestData.get(0).getStrInputCountry(),
+                uTestData.get(0).getStrPassword(),
+                uTestData.get(0).getStrConfirmPassword()
+        ));
 
     }
 
     @Then("^he opens the registration (.*)$")
-    public void heOpensTheRegistrationForm(String question) {
-        OnStage.theActorInTheSpotlight().should(GivenWhenThen.seeThat(Answer.toThe(question)));
+    public void heOpensTheRegistrationForm(List<UTestData> uTestData) throws Exception {
+        OnStage.theActorInTheSpotlight().should(GivenWhenThen.seeThat(Answer.toThe(uTestData.get(0).getStrFirstName())));
     }
 
 }
